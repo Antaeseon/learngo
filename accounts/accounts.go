45e8847c6fd0ffe85d3cc6e5b6708e29db45b905
Antaeseon <ridgnfu@naver.com>
2020-09-06 21:27:22 +0900

    finish method

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,10 +1,17 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
 //Account struct
 type Account struct {
 	owner   string
 	balance int
 }
+
+var errNoMoney = errors.New("Can't Withdraw")
 
 //NewAccount creates account
 func NewAccount(owner string) *Account {
@@ -13,11 +20,34 @@ func NewAccount(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
 // Balance of your account
 func (a Account) Balance() int {
 	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.Balance() < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
+
+// ChangeOwner of the Account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+//Owner of the accounts
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas:", a.Balance())
 }
