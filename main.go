9393911eee69854c086fe580367d6e690d787cd7
Antaeseon <ridgnfu@naver.com>
2020-09-08 00:19:13 +0900

    make csvfile with goroutine

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -22,18 +24,56 @@ var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50&start=0"
 
 func main() {
 	var jobs []extractedJob
+	c := make(chan []extractedJob)
 	totlaPages := getPages()
 	fmt.Println("페이지 : ", totlaPages)
 
 	for i := 0; i < totlaPages; i++ {
-		extratedJobs := getPage(i)
-		jobs = append(jobs, extratedJobs...)
+		go getPage(i, c)
 	}
-	fmt.Println(jobs)
+
+	for i := 0; i < totlaPages; i++ {
+		extractedJobs := <-c
+		jobs = append(jobs, extractedJobs...)
+	}
+
+	writeJobs(jobs)
+	fmt.Println("Done, extracted", len(jobs))
 }
 
-func getPage(page int) []extractedJob {
+func writeJobs(jobs []extractedJob) {
+	c := make(chan []string)
+
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"ID", "Title", "LOCATION", "Salary", "Summary"}
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		go writeJobDetail(job, c)
+	}
+
+	for i := 0; i < len(jobs); i++ {
+		jobData := <-c
+		writeErr := w.Write(jobData)
+		checkErr(writeErr)
+	}
+
+}
+
+func writeJobDetail(job extractedJob, c chan<- []string) {
+	const jobURL = "https://kr.indeed.com/viewjob?jk="
+	c <- []string{jobURL + job.id, job.title, job.location, job.salary, job.summary}
+}
+
+func getPage(page int, mainC chan<- []extractedJob) {
 	var jobs []extractedJob
+	c := make(chan extractedJob)
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
@@ -44,10 +84,15 @@ func getPage(page int) []extractedJob {
 
 	serchCards := doc.Find(".jobsearch-SerpJobCard")
 	serchCards.Each(func(i int, card *goquery.Selection) {
-		job := extractJob(card)
-		jobs = append(jobs, job)
+		go extractJob(card, c)
 	})
-	return jobs
+
+	for i := 0; i < serchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+
+	mainC <- jobs
 }
 
 func getPages() int {
@@ -78,20 +123,19 @@ func checkCode(res *http.Response) {
 
 }
 
-func extractJob(card *goquery.Selection) extractedJob {
+func extractJob(card *goquery.Selection, c chan<- extractedJob) {
 	id, _ := card.Attr("data-jk")
 	title := cleanString(card.Find(".title>a").Text())
 	location := cleanString(card.Find(".sjcl").Text())
 	salary := cleanString(card.Find(".salaryText").Text())
 	summary := cleanString(card.Find(".summary").Text())
-	return extractedJob{
+	c <- extractedJob{
 		id:       id,
 		title:    title,
 		location: location,
 		salary:   salary,
 		summary:  summary,
 	}
-
 }
 
 func cleanString(str string) string {
